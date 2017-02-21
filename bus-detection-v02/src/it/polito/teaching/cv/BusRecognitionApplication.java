89f23fccdb6355e85875934406e9b22fd3b0e5ed
canerknowit <caner.eren@knowit.se>
2017-02-21 18:05:32 +0100

    Initial repository created

---
+++ bus-detection-v02/src/it/polito/teaching/cv/BusRecognitionApplication.java
@@ -0,0 +1,50 @@
+package it.polito.teaching.cv;
+
+import org.opencv.core.Core;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
+import javafx.fxml.FXMLLoader;
+
+public class BusRecognitionApplication extends Application
+{
+	/**
+	 * The main class for a JavaFX application. It creates and handles the main
+	 * window with its resources (style, graphics, etc.).
+	 * 
+	 */
+	@Override
+	public void start(Stage primaryStage)
+	{
+		try
+		{
+			// load the FXML resource
+			BorderPane root = (BorderPane) FXMLLoader.load(getClass().getResource("BusRecognition.fxml"));
+			// set a whitesmoke background
+			root.setStyle("-fx-background-color: whitesmoke;");
+			// create and style a scene
+			Scene scene = new Scene(root, 800, 600);
+			scene.getStylesheets().add(getClass().getResource("application.css").toExternalForm());
+			// create the stage with the given title and the previously created
+			// scene
+			primaryStage.setTitle("Object Recognition");
+			primaryStage.setScene(scene);
+			// show the GUI
+			primaryStage.show();
+		}
+		catch (Exception e)
+		{
+			e.printStackTrace();
+		}
+	}
+	
+	public static void main(String[] args)
+	{
+		// load the native OpenCV library
+		System.loadLibrary(Core.NATIVE_LIBRARY_NAME);
+		
+		launch(args);
+	}
+}
